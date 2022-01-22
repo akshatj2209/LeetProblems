c94c213404efa2a46b504bd2ed57bd1554ed948e
Akshat Joshi <57488922+akshatj2209@users.noreply.github.com>
2022-01-22 22:55:58 +0530

    Time: 46 ms (66.74%), Space: 7.6 MB (80.58%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,30 +1,30 @@
 class Solution {
-   public:
-      bool isMatch(string s, string p) {
-          int m=s.length(),n=p.length();
-          bool dp[m+1][n+1];
-         memset(dp, false, sizeof(dp));
-         int i, j;
-         if (n == 0 && m != 0) {
-            return false;
-         }
-         dp[0][0] = true;
-         for (i = 1; i <= n; i++) {
-            if (p[i - 1] != '*') {
-                break;
+    public:
+        bool isMatch(string s, string p) {
+            int m = s.length(), n = p.length();
+            bool dp[m + 1][n + 1];
+            memset(dp, false, sizeof(dp));
+            int i, j;
+            if (n == 0 && m != 0) {
+                return false;
             }
-            dp[0][i] = true;
-         }
-         for (i = 1; i <= m; i++) {
-            for (j = 1; j <= n; j++) {
-               if (s[i - 1] == p[j - 1] || p[j - 1] == '?') {
-                  dp[i][j] = dp[i - 1][j - 1];
-               }
-               if (p[j - 1] == '*') {
-                  dp[i][j] = dp[i - 1][j - 1] || dp[i][j - 1] || dp[i - 1][j];
-               }
+            dp[0][0] = true;
+            for (i = 1; i <= n; i++) {
+                if (p[i - 1] != '*') {
+                    break;
+                }
+                dp[0][i] = true;
             }
-         }
-         return dp[m][n];
-      }
+            for (i = 1; i <= m; i++) {
+                for (j = 1; j <= n; j++) {
+                    if (s[i - 1] == p[j - 1] || p[j - 1] == '?') {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+                    if (p[j - 1] == '*') {
+                        dp[i][j] = dp[i - 1][j - 1] || dp[i][j - 1] || dp[i - 1][j];
+                    }
+                }
+            }
+            return dp[m][n];
+        }
 };
