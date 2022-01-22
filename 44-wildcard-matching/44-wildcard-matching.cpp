304910d9eed6c82ebf1802ee9098eac1d98d4b08
Akshat Joshi <57488922+akshatj2209@users.noreply.github.com>
2022-01-22 22:39:57 +0530

    Time: 100 ms (32.62%), Space: 11.3 MB (42.65%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,36 +1,3 @@
-bool recur(string s, string p, int i, int j,vector<vector<int>> &memo){
-    int c=false;
-    if(memo[i][j]!=2){
-        return memo[i][j];
-    }
-    if(i>=s.length()&&j>=p.length()){
-        return true;
-    }
-    if(j>=p.length()){
-        return false;
-    }
-    if(p[j]=='*' && i>=s.length()){
-        c=c||recur(s,p,i,j+1,memo);
-        return c;
-    }
-    if(i>=s.length()){
-        return false;
-    }
-    
-    if(i<s.length() &&j<p.length() &&  (s[i]==p[j] || p[j]=='?')){
-        c=c||recur(s,p,i+1,j+1,memo);
-    }
-    if(j<p.length() && p[j]=='*'){
-        c=c||recur(s,p,i,j+1,memo);
-        c=c||recur(s,p,i+1,j+1,memo);
-        c=c||recur(s,p,i+1,j,memo);
-    }
-    if(j<p.length() && p[j]!='*' && p[j]!='?' && s[i]!=p[j]){
-        return false;
-    }
-    memo[i][j]=c;
-    return memo[i][j];
-}
 
 
 class Solution {
@@ -42,20 +9,10 @@ public:
             return false;
         }
         dp[0][0]=true;
-        bool dec=false;
-        for(j=0;j<p.length();j++){
-            if(p[j]!='*'){
-                dec=true;
-                break;
-            }
-        }
-        if(!dec){
-            return true;
-        }
+        bool dec=true;
         for(i=1;i<=s.length();i++){
             dp[i][0]=false;
         }
-        dec=true;
         for(i=1;i<=p.length();i++){
             if(p[i-1]!='*'){
                 dec=false;
