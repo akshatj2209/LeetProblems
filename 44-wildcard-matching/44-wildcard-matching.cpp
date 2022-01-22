a6ec89e86bd79c6d6f76177499e959623c3e208f
Akshat Joshi <57488922+akshatj2209@users.noreply.github.com>
2022-01-22 22:46:05 +0530

    Time: 239 ms (13.57%), Space: 11.4 MB (36.18%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -25,7 +25,7 @@ public:
                     dp[i][j] = dp[i-1][j-1];
                 }
                 if(p[j-1]=='*'){
-                    dp[i][j] = dp[i-1][j-1]|| dp[i][j-1]|| dp[i-1][j];
+                    dp[i][j] = dp[i-1][j-1]| dp[i][j-1]| dp[i-1][j];
                 }
             }
         }
