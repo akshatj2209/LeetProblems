4f16260a1cfa4865b14808c9be72c8b6ea206ed6
Akshat Joshi <57488922+akshatj2209@users.noreply.github.com>
2022-01-23 13:02:03 +0530

    Time: 16 ms (54.59%), Space: 9 MB (51.65%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -24,7 +24,28 @@ int recur(string word1, string word2, int i, int j,vector<vector<int>> &memo){
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        vector<vector<int>> memo(word1.length()+1, vector<int>(word2.length()+1,-1));
-        return recur(word1,word2,0,0,memo);
+        vector<vector<int>> dp(word1.length()+1, vector<int>(word2.length()+1,0));
+        int m=word1.length();
+        int n=word2.length();
+        int i,j;
+        for(i=0;i<=m;i++){
+            for(j=0;j<=n;j++){
+                if(i==0){
+                    dp[0][j]=j;
+                }
+                else if(j==0){
+                    dp[i][0]=i;
+                }
+                else{
+                    if(word1[i-1]==word2[j-1]){
+                        dp[i][j]=dp[i-1][j-1];
+                    }
+                    else{
+                        dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+                    }
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
