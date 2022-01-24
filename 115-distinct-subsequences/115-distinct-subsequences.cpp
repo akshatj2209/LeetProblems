98c36a0c7e0cc3bec2fdf91204c07fa337faca27
Akshat Joshi <57488922+akshatj2209@users.noreply.github.com>
2022-01-24 23:03:56 +0530

    Time: 16 ms (85.23%), Space: 15.2 MB (60.29%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,20 +1,5 @@
 class Solution {
 public:
-    int recur(string a, string b, int i, int j,vector<vector<int>> &memo){
-        if(memo[i][j]!=-1){
-            return memo[i][j];
-        }
-        if(j==b.length()){
-            return memo[i][j]=1;
-        }
-        if(i==a.length()){
-            return memo[i][j]=0;
-        }
-        if(a[i]==b[j]){
-            return memo[i][j]=recur(a,b,i+1,j+1,memo)+recur(a,b,i+1,j,memo);
-        }
-        return memo[i][j]=recur(a,b,i+1,j,memo);
-    }
     int numDistinct(string s, string t) {
         unsigned long long int dp [s.length()+1][t.length()+1];
         int m=s.length();
@@ -25,11 +10,8 @@ public:
                 if(i==0 && j==0){
                     dp[i][j]=1;
                 }
-                else if(i==0){
-                    dp[i][j]=0;
-                }
-                else if (j==0){
-                     dp[i][j]=1;                  
+                else if(i==0 || j==0){
+                    dp[i][j]=((j==0)?1:0);
                 }
                 else if(s[i-1]==t[j-1]){
                     dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
