17412c7019715343b6fda22a85656fbbf45be501
Akshat Joshi <57488922+akshatj2209@users.noreply.github.com>
2022-01-24 12:59:55 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (69.90%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,27 +1,5 @@
 class Solution {
     public:
-        unordered_map < string, int > memo;
-    int recur(string s) {
-        if (memo.find(s) != memo.end()) {
-            return memo[s];
-        }
-        if (s == "" || s[0] == '0') {
-            return memo[s] = 0;
-        }
-        if (s.length() == 1) {
-            return memo[s] = 1;
-        }
-        if (s.length() == 2 && stoi(s.substr(0, 2)) <= 26) {
-            memo[s] = 1 + recur(s.substr(1));
-            return memo[s];
-        }
-        if (stoi(s.substr(0, 2)) <= 26) {
-            memo[s] = recur(s.substr(2)) + recur(s.substr(1));
-            return memo[s];
-        }
-        memo[s] = recur(s.substr(1));
-        return memo[s];
-    }
         int numDecodings(string s) {
             int i;
             int n=s.length();
