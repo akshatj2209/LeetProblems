a2cbe9ed8bcff9bfa512409c6ec9d3fce907674d
Akshat Joshi <57488922+akshatj2209@users.noreply.github.com>
2022-01-24 19:01:45 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (94.37%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,30 +1,8 @@
 class Solution {
 public:
-    bool recur(string s1, string s2, string s3, int i, int j, vector<vector<int>> &memo){
-        if(memo[i][j]!=2){
-            return memo[i][j];
-        }
-        if(i==s1.length() && j==s2.length() && i+j==s3.length()){
-            return memo[i][j]=true;
-        }
-        if(i+j==s3.length()){
-            return memo[i][j]=false;
-        }
-        if(i<s1.length() && s1[i]==s3[i+j] && j<s2.length() && s2[j]==s3[i+j]){
-            return memo[i][j]=recur(s1,s2,s3,i+1,j,memo) || recur(s1,s2,s3,i,j+1,memo);
-        }
-        if(i<s1.length() && s1[i]==s3[i+j]){
-            return memo[i][j]=recur(s1,s2,s3,i+1,j,memo);
-        }
-        if(j<s2.length() && s2[j]==s3[i+j]){
-            return memo[i][j]=recur(s1,s2,s3,i,j+1,memo);
-        }
-        return memo[i][j]=false;
-        
-    }
     bool isInterleave(string s1, string s2, string s3) {
-        vector<vector<int>> memo(s1.length()+1,vector<int>(s2.length()+1,2));
-        vector<vector<bool>> dp(s1.length()+1,vector<bool>(s2.length()+1,false));
+        bool dp[s1.length()+1][s2.length()+1];
+        memset(dp,false,sizeof(dp));
         int m=s1.length(), n=s2.length();
         int i,j;
         if(m+n!=s3.length()){
@@ -39,16 +17,10 @@ public:
                     if(s2[j-1]==s3[j-1]){
                         dp[i][j]=dp[i][j-1];
                     }
-                    else{
-                        dp[i][j]=false;
-                    }
                 }
                 else if(j==0){
                     if(s1[i-1]==s3[i-1]){
                         dp[i][j]=dp[i-1][j];
-                    }
-                    else{
-                        dp[i][j]=false;
                     }
                 }
                 else{
@@ -60,9 +32,6 @@ public:
                     }
                     else if(s1[i-1]==s3[i+j-1]){
                         dp[i][j]=dp[i-1][j];
-                    }
-                    else{
-                        dp[i][j]=false;
                     }
                     
                 }
