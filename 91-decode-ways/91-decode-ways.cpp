e5eb639fc059dab3f61a5098e00151c5e574f484
Akshat Joshi <57488922+akshatj2209@users.noreply.github.com>
2022-01-24 12:46:20 +0530

    Time: 0 ms (100.00%), Space: 17.8 MB (5.01%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,11 +1,11 @@
 class Solution {
     public:
         unordered_map < string, int > memo;
-    int numDecodings(string s) {
+        int numDecodings(string s) {
 
-        return recur(s);
-    }
-    int recur(string s) {
+            return recur(s);
+        }
+        int recur(string s) {
         if (memo.find(s) != memo.end()) {
             return memo[s];
         }
@@ -19,7 +19,7 @@ class Solution {
             memo[s] = 1 + recur(s.substr(1));
             return memo[s];
         }
-        if (s.length() > 2 && stoi(s.substr(0, 2)) <= 26) {
+        if (stoi(s.substr(0, 2)) <= 26) {
             memo[s] = recur(s.substr(2)) + recur(s.substr(1));
             return memo[s];
         }
