fc63921c5eab1f89bde00fe59a5551a4f5c6b826
Akshat Joshi <57488922+akshatj2209@users.noreply.github.com>
2022-01-24 23:04:07 +0530

    Time: 33 ms (61.14%), Space: 15.2 MB (57.39%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -11,7 +11,7 @@ public:
                     dp[i][j]=1;
                 }
                 else if(i==0 || j==0){
-                    dp[i][j]=((j==0)?1:0);
+                    dp[i][j]=(j==0);
                 }
                 else if(s[i-1]==t[j-1]){
                     dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
