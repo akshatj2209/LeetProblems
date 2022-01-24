b322f03d31e60f9bd938997fc8029ab7bba65b41
Akshat Joshi <57488922+akshatj2209@users.noreply.github.com>
2022-01-24 18:26:53 +0530

    Time: 48 ms (14.58%), Space: 27.7 MB (12.92%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    bool recur(string s1, string s2, string s3, int i, int j, int k,vector<vector<vector<int>>> &memo){
-        if(memo[i][j][k]!=2){
-            return memo[i][j][k];
+    bool recur(string s1, string s2, string s3, int i, int j, vector<vector<int>> &memo){
+        if(memo[i][j]!=2){
+            return memo[i][j];
         }
-        if(i==s1.length() && j==s2.length() && k==s3.length()){
-            return memo[i][j][k]=true;
+        if(i==s1.length() && j==s2.length() && i+j==s3.length()){
+            return memo[i][j]=true;
         }
-        if(k==s3.length()){
-            return memo[i][j][k]=false;
+        if(i+j==s3.length()){
+            return memo[i][j]=false;
         }
-        if(i<s1.length() && s1[i]==s3[k] && j<s2.length() && s2[j]==s3[k]){
-            return memo[i][j][k]=recur(s1,s2,s3,i+1,j,k+1,memo) || recur(s1,s2,s3,i,j+1,k+1,memo);
+        if(i<s1.length() && s1[i]==s3[i+j] && j<s2.length() && s2[j]==s3[i+j]){
+            return memo[i][j]=recur(s1,s2,s3,i+1,j,memo) || recur(s1,s2,s3,i,j+1,memo);
         }
-        if(i<s1.length() && s1[i]==s3[k]){
-            return memo[i][j][k]=recur(s1,s2,s3,i+1,j,k+1,memo);
+        if(i<s1.length() && s1[i]==s3[i+j]){
+            return memo[i][j]=recur(s1,s2,s3,i+1,j,memo);
         }
-        if(j<s2.length() && s2[j]==s3[k]){
-            return memo[i][j][k]=recur(s1,s2,s3,i,j+1,k+1,memo);
+        if(j<s2.length() && s2[j]==s3[i+j]){
+            return memo[i][j]=recur(s1,s2,s3,i,j+1,memo);
         }
-        return memo[i][j][k]=false;
+        return memo[i][j]=false;
         
     }
     bool isInterleave(string s1, string s2, string s3) {
-        vector<vector<vector<int>>> memo(s1.length()+1,vector<vector<int>>(s2.length()+1,vector<int>(s3.length()+1,2)));
-        return recur(s1,s2,s3,0,0,0,memo);   
+        vector<vector<int>> memo(s1.length()+1,vector<int>(s2.length()+1,2));
+        return recur(s1,s2,s3,0,0,memo);   
     }
 };
