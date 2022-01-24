871fba0fd3e3fe045447eded33aab1db2135bd20
Akshat Joshi <57488922+akshatj2209@users.noreply.github.com>
2022-01-24 19:12:20 +0530

    Time: 4 ms (71.28%), Space: 6.1 MB (94.37%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,42 +1,29 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        bool dp[s1.length()+1][s2.length()+1];
+        bool dp[s2.length()+1];
         memset(dp,false,sizeof(dp));
         int m=s1.length(), n=s2.length();
-        int i,j;
+        int i=0,j=0;
         if(m+n!=s3.length()){
             return false;
         }
         for(i=0;i<=m;i++){
             for(j=0;j<=n;j++){
                 if(i==0 && j==0){
-                    dp[i][j]=true;
+                    dp[j]=true;
                 }
                 else if(i==0){
-                    if(s2[j-1]==s3[j-1]){
-                        dp[i][j]=dp[i][j-1];
-                    }
+                    dp[j]=dp[j-1] && s2[j-1]==s3[j-1];    
                 }
                 else if(j==0){
-                    if(s1[i-1]==s3[i-1]){
-                        dp[i][j]=dp[i-1][j];
-                    }
+                    dp[j]=dp[j] && s1[i-1]==s3[i-1];
                 }
                 else{
-                    if(s1[i-1]==s3[i+j-1] && s2[j-1]==s3[i+j-1]){
-                        dp[i][j]=dp[i-1][j] || dp[i][j-1];
-                    }
-                    else if(s2[j-1]==s3[i+j-1]){
-                        dp[i][j]=dp[i][j-1];
-                    }
-                    else if(s1[i-1]==s3[i+j-1]){
-                        dp[i][j]=dp[i-1][j];
-                    }
-                    
+                    dp[j]=(dp[j] && s1[i-1]==s3[i+j-1]) || (dp[j-1] && s2[j-1]==s3[i+j-1]);
                 }
             }
         }
-        return dp[m][n];   
+        return dp[n];   
     }
 };
