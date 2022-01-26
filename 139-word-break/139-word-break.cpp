e1a28f2bfb7947b48e4c335feb5422e028ebe174
Akshat Joshi <57488922+akshatj2209@users.noreply.github.com>
2022-01-26 20:42:38 +0530

    Time: 24 ms (32.09%), Space: 14.4 MB (26.48%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        bool dp[s.length()+1];
+        memset(dp,false,sizeof(dp));
+        dp[0]=true;
+        for(int i=0;i<s.length();i++){
+            for(int j=0;j<=i;j++){
+                if(find(wordDict.begin(), wordDict.end(),s.substr(j,i-j+1)) != wordDict.end()){
+                    cout<<i+1<<" "<<j<<endl;
+                    dp[i+1]= dp[i+1] || dp[j];
+                }
+            }
+        }
+        return dp[s.length()];
+        
+    }
+};
