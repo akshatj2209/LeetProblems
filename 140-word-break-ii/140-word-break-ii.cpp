5592852c4ec3b374e6aa896ce1b7b613837e679e
Akshat Joshi <57488922+akshatj2209@users.noreply.github.com>
2022-01-27 11:42:29 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (66.11%) - LeetHub

---
+++ 140-word-break-ii/140-word-break-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void recur(string s, vector<string>& wordDict, vector<string>& ans, int i, string comp){
+        cout<<i<<" "<<comp<<endl;
+        if(i==s.length()){
+            comp.pop_back();
+            ans.push_back(comp);
+        }
+        for(int j=0;j<wordDict.size();j++){
+            if(s.substr(i,wordDict[j].length()) == wordDict[j]){
+                recur(s,wordDict,ans,i+wordDict[j].length(),comp+wordDict[j]+ " ");
+            }
+        }
+        return;
+    }
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        vector<string> ans;
+        recur(s,wordDict,ans,0,"");
+        return ans;
+    }
+};
