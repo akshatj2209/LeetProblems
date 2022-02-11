3905dd73efd21c7e4d4c5e4f765aaa34983273e4
Akshat Joshi <57488922+akshatj2209@users.noreply.github.com>
2022-02-11 17:03:34 +0530

    Time: 93 ms (78.57%), Space: 5.9 MB (97.76%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int numSquares(int n) {
         int dp[n+1];
-        memset(dp,-1,sizeof(dp));
+        memset(dp,1000000,sizeof(dp));
         int i,j;
         dp[0]=0;
         for(i=1;i<=n;i++){
             for(j=1;j*j<=i;j++){
-                if(dp[i]==-1){
-                    dp[i]=1+dp[i-j*j];
-                }
-                else
+                // if(dp[i]==-1){
+                //     dp[i]=1+dp[i-j*j];
+                // }
+                // else
                 dp[i]=min(dp[i], 1+dp[i-j*j]);
             }
         }
