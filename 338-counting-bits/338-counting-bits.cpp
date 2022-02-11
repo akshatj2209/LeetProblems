71dfd012b41eb4c1a7ca1272a400d20f407e8ccc
Akshat Joshi <57488922+akshatj2209@users.noreply.github.com>
2022-02-11 23:20:40 +0530

    Time: 4 ms (89.23%), Space: 7.9 MB (75.02%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> dp(n+1,0);
         for(int i=1;i<=n;i++){
             if(i%2!=0){
-                dp[i]=dp[i/2]+1;
+                dp[i]=dp[i-1]+1;
             }
             else{
                 dp[i]=dp[i/2];
