b6318bba5d3ec5b283a99d5ce9bfb2b0e2401815
Akshat Joshi <57488922+akshatj2209@users.noreply.github.com>
2022-02-11 17:00:45 +0530

    Time: 100 ms (77.66%), Space: 5.8 MB (99.81%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -16,6 +16,21 @@ public:
     int numSquares(int n) {
         int memo[n+1];
         memset(memo,-1,sizeof(memo));
-        return recur(n,memo);
+        int dp[n+1];
+        memset(dp,-1,sizeof(dp));
+        int i,j;
+        for(i=0;i<=n;i++){
+            if(i==0){
+                dp[i]=0;
+            }
+            for(j=1;j*j<=i;j++){
+                if(dp[i]==-1){
+                    dp[i]=1+dp[i-j*j];
+                }
+                else
+                dp[i]=min(dp[i], 1+dp[i-j*j]);
+            }
+        }
+        return dp[n];
     }
 };
