1d92335f8a974ef45bead38b07bc18d759bf8f5f
Akshat Joshi <57488922+akshatj2209@users.noreply.github.com>
2022-02-13 15:07:16 +0530

    Time: 4 ms (22.18%), Space: 5.9 MB (72.66%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
-    int recur(int n, int memo[60]){
-        if(memo[n]!=-1){
-            return memo[n];
-        }
-        int ans=1;
-        for(int i=1;i<=n;i++){
-            int t=i*recur(n-i,memo);
-            ans=max(ans,t);
-        }
-        return memo[n]=ans;
-    }
     int integerBreak(int n) {
-        int ans=1;
-        int memo[60];
-        memset(memo,-1,sizeof(memo));
-        memo[0]=1;
-        for(int i=1;i<n;i++){
-            int t=i*recur(n-i, memo);
-            ans=max(ans,t);
-        }
         int dp[n+1];
         memset(dp,0,sizeof(dp));
         dp[0]=0;
