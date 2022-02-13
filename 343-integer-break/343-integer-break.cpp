3e6060f640bff4890642e7dc1e2e5f88b8175d06
Akshat Joshi <57488922+akshatj2209@users.noreply.github.com>
2022-02-13 15:07:00 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (89.39%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -20,6 +20,16 @@ public:
             int t=i*recur(n-i, memo);
             ans=max(ans,t);
         }
-        return ans;
+        int dp[n+1];
+        memset(dp,0,sizeof(dp));
+        dp[0]=0;
+        dp[1]=1;
+        int i, j;
+        for(i=2;i<=n;i++){
+            for(j=1;j<i;j++){
+                dp[i]=max(dp[i], max((i-j) * j,(i-j) *dp[j]));
+            }
+        }
+        return dp[n];
     }
 };
