1910b955ae0cca681a6763665a5674341783aeaf
Akshat Joshi <57488922+akshatj2209@users.noreply.github.com>
2022-02-16 11:26:15 +0530

    Time: 55 ms (11.48%), Space: 26.5 MB (6.65%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -3,16 +3,17 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int product=1;
         vector<int> prefix;
-        vector<int> ans(nums.size());
+        vector<int> ans;
         for(int i=0;i<nums.size();i++){
             prefix.push_back(product);
             product *= nums[i];
         }
         product=1;
         for(int i=nums.size()-1;i>=0;i--){
-            ans[i]=product*prefix[i];
+            ans.push_back(product*prefix[i]);
             product *= nums[i];
         }
+        reverse(ans.begin(),ans.end());
         return ans;
         
     }
